6fd430afe5cd5218bbd4a64e07071c9b37293e6d
Carlos Miguel <carlosmiguel040303@hotmail.com>
2025-11-15 14:44:24 -0300

    Atualizando requirements

--- script_pharma/api.py
+++ script_pharma/api.py
@@ -78,4 +78,4 @@ def buscar_remedio_cep(cep:str):
     return {"resultados": resultados_farma}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
