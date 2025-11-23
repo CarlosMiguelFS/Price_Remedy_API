8851a65b81962c7d7bad16f4bfb4b3e97c1a343e
Carlos Miguel <carlosmiguel040303@hotmail.com>
2025-11-23 18:04:00 -0300

    Atualização

--- script_pharma/drograria_sao_paulo.py
+++ script_pharma/drograria_sao_paulo.py
@@ -1,6 +1,6 @@
 def check_drogaria_sao_paulo(cep,produto):
     import httpx
-
+    product_description = {}
     d_para = {
         "Mounjaro 2,5mg/ml": "887528",
         "Mounjaro 5mg/ml": "887455",
@@ -23,9 +23,11 @@ def check_drogaria_sao_paulo(cep,produto):
     headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 OPR/123.0.0.0"}
     endereco = httpx.post("https://www.drogariasaopaulo.com.br/api/checkout/pub/orderforms/simulation", json=payload, headers=headers).json()
 
-    for values in endereco.get("pickupPoints",[]):
-        print(values)
-        if values.get("address"):
-            return {"loja": "Drogaria São Paulo", "endereco": dict(values["address"])} 
-
+    if endereco.get("logisticsInfo"):
+        for product in endereco["logisticsInfo"][0]["slas"]:
+            if product.get("pickupStoreInfo", {}).get("address"):
+                product_description["endereco"] = product["pickupStoreInfo"]["address"]  
+                product_description["value"] = float(endereco["items"][0]["price"]/100)
+                product_description["loja"] = "Pacheco"
+                return product_description
     return None
