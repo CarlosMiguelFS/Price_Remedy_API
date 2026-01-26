424c3e7bf5a82a6c3a707e326e8ae0fb1bea70b6
CarlosMiguel0403 <carlos.miguel@efizi.com.br>
2026-01-26 11:44:12 -0300

    Atualizando outra plataforma base44

--- script_pharma/api.py
+++ script_pharma/api.py
@@ -18,7 +18,8 @@ origins = [
     "https://sonar-med.base44.app",
     "https://app.base44.com/apps/69190e47002195fa7c0dd7ec/editor/preview/Home",
     "https://aistudio.google.com/apps/drive/1zIg7ybuKEFCR9KqNtoPSwzdDypvryZK1?showAssistant=true&showPreview=true&resourceKey=",
-    "https://radar-meds.lovable.app"
+    "https://radar-meds.lovable.app",
+    "https://med-location.base44.app"
 ]
 
 app.add_middleware(
