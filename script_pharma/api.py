c76b1d7148188852dc4b4630e91818e4da33c935
carlosmiguel0403 <carlosmiguel040303@hotmail.com>
2026-05-19 21:10:41 -0300

    Atualizando remedios

--- script_pharma/api.py
+++ script_pharma/api.py
@@ -18,7 +18,7 @@ origins = [
     "https://med-radar-nine.vercel.app",
     "https://med-location.base44.app",
     "http://localhost:3000",
-    "https://radar-medicamentos.base44.app/",
+    "https://radar-medicamentos.base44.app",
 ]
 
 app.add_middleware(
